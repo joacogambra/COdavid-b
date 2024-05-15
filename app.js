f651b0e845005e42a9cd3dbd98a7be73ce00f9da
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2024-05-15 10:23:08 -0300

    cors policy

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+// app.use(cors());
+
+const corsOptions = {
+  origin: "https://tradesinlimites.com.ar",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/", indexRouter);
 
